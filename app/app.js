6d0ef786f696d1e5c473dacba3418ddd657ab8cc
alex kang <akang2@gmail.com>
2016-11-09 12:56:46 -0800

    commented out node only version

--- app/app.js
+++ app/app.js
@@ -1,8 +1,13 @@
-var http = require('http');
 
-var myServer = http.createServer(function(request, response) {
 
-});
 
-myServer.listen(3000);
-console.log('Go to http://localhost:3000 on your browser');
+// var http = require('http');
+//
+// var myServer = http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type" : "text/html"});
+//   response.write('<h1>Roux Meetups</h1>');
+//   response.end();
+// });
+//
+// myServer.listen(3000);
+// console.log('Go to http://localhost:3000 on your browser');

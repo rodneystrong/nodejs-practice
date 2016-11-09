2da2f98c29ff94d2a49b880311da28ffb4ce5d7f
alex kang <akang2@gmail.com>
2016-11-09 12:53:46 -0800

    added port and console log

--- app/app.js
+++ app/app.js
@@ -3,3 +3,6 @@ var http = require('http');
 var myServer = http.createServer(function(request, response) {
 
 });
+
+myServer.listen(3000);
+console.log('Go to http://localhost:3000 on your browser');
